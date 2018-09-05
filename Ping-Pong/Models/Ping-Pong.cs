36f9a499cbce2bdfa736193cb70116245ae681bd
Catherine Bradley and Meria Thomas <meria@>
2018-09-05 11:15:16 -0700

    add front end logic to take input and output a range of a number

--- Ping-Pong/Models/Ping-Pong.cs
+++ Ping-Pong/Models/Ping-Pong.cs
@@ -1,41 +1,47 @@
-// using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace PingPong
-{
+// namespace PingPong
+// {
   public class PingPongGenerator
   {
-    // public int input;
-    public string IsPingPong(int input)
+    public int n;
+    public string IsPingPong(int n)
     {
-      if (input % 3 == 0 && input % 5 == 0)
+      if (n % 3 == 0 && n % 5 == 0)
       {
         return "ping-pong";
       }
-      else if (input % 5 == 0)
+      else if (n % 5 == 0)
       {
         return "pong";
       }
-      else if (input % 3 == 0)
+      else if (n % 3 == 0)
       {
         return "ping";
       }
       else
       {
-        return "the number";
+        return n.ToString();
       }
     }
   }
-}
-
-// class Program
-// {
-//   public static void Main()
-//   {
-//     Console.WriteLine("Please enter a number");
-//     int input = int.Parse(Console.ReadLine());
-//
-//     PingPongGenerator checkPingPong = new PingPongGenerator();
-//
-//     Console.WriteLine(checkPingPong.IsPingPong(input));
-//   }
 // }
+
+class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Please enter a number");
+    int input = int.Parse(Console.ReadLine());
+    Console.WriteLine("Here are the results! Ping-Pong!");
+
+    IEnumerable<int> inputRange = Enumerable.Range(1, input);
+    foreach (int n in inputRange)
+    {
+      PingPongGenerator checkPingPong = new PingPongGenerator();
+      Console.WriteLine(checkPingPong.IsPingPong(n));
+    }
+  }
+}
